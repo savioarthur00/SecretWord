faad3458ecae6375a9109880059c9f963bfce11a
savioarthur00 <savio1281@gmail.com>
2023-08-07 20:40:46 -0300

    build teste game

---
+++ src/data/words.jsx
@@ -0,0 +1,71 @@
+export const wordsList = {
+  carro: [
+    "Motor", "Porta", "Capô", "Pneu", "Antena",
+     "Volante", "Freio", "Retrovisor", "Farol", "Chave",
+    "Cinto", "Vidro", "Buzina", "Radiador", "Estepe",
+    "Para-choque", "Escapamento", "Suspensão", "Câmbio", "Vela",
+    "Filtro", "Embreagem", "Correia", "Carburador", "Tanque",
+    "Seta", "Friso", "Capota", "Insulfilm", "Trava",
+    "Alarme", "Pára-brisa", "Palheta", "Limpador", "Bateria",
+    "Ignição", "Cilindro", "Amortecedor", "Extintor", "Direção",
+    "Porta-malas", "Radiador", "Bateria", "Vidro elétrico", "Airbag",
+    "Janela", "Eixo", "Motor de arranque", "Freio a disco", "Cárter"
+  ],
+  fruta: [
+    "Banana", "Maçã", "Pêra", "Mamão", "Laranja",
+     "Uva", "Abacaxi", "Melancia", "Goiaba", "Caju",
+    "Morango", "Limão", "Ameixa", "Kiwi", "Melão",
+    "Jabuticaba", "Acerola", "Cereja", "Coco", "Groselha",
+    "Romã", "Tangerina", "Amora", "Pêssego", "Abacate",
+    "Maracujá", "Cajá", "Figo", "Pitanga", "Açaí",
+    "Framboesa", "Jaca", "Pera", "Fruta do conde", "Atemoia",
+    "Graviola", "Goiaba", "Carambola", "Mexerica", "Cereja",
+    "Lichia", "Pinha", "Nêspera", "Rambutan", "Mirtilo"
+  ],
+  corpo: [
+    "Braço", "Perna", "Cérebro", "Pescoço", "Olhos",
+     "Orelha", "Cotovelo", "Mão", "Pulso", "Joelho",
+    "Tornozelo", "Tornozelo", "Tornozelo", "Quadril", "Canela",
+    "Coxa", "Cabeça", "Nariz", "Lábio", "Sobrancelha",
+    "Queixo", "Queixo", "Queixo", "Queixo", "Dente",
+    "Pele", "Pele", "Pele", "Pele", "Cabelo",
+    "Unha", "Unha", "Unha", "Unha", "Dedo do pé",
+    "Costas", "Costas", "Costas", "Costas", "Pescoço",
+    "Cintura", "Cintura", "Cintura", "Cintura", "Axila",
+    "Nádegas", "Nádegas", "Nádegas", "Nádegas", "Sobrancelha"
+  ],
+  computador: [
+    "Mouse", "Teclado", "Monitor", "Gabinete",
+     "Placa-mãe", "Processador", "Memória", "HD", "SSD",
+    "Gravador", "Placa de vídeo", "Fonte", "Cooler", "Ventoinha",
+    "Porta USB", "Conector", "Wi-Fi", "Bluetooth", "Ethernet",
+    "Bateria", "Tela", "Webcam", "Alto-falante", "Microfone",
+    "Touchpad", "Tela sensível ao toque", "Leitor de cartão", "Scanner", "Impressora",
+    "Projetor", "Adaptador", "Hub USB", "Hub USB", "Hub USB",
+    "Fone de ouvido", "Caixa de som", "Rato", "Rato", "Rato",
+    "Teclado numérico", "Teclado sem fio", "Teclado mecânico", "Teclado retroiluminado", "Teclado ergonômico",
+    "Mouse pad", "Mouse pad", "Mouse pad", "Mouse pad", "Mouse pad"
+  ],
+  programação: [
+    "Linguagem", "Framework", "JavaScript", "React",
+     "Python", "Java", "C#", "C++", "Ruby",
+    "Swift", "PHP", "HTML", "CSS", "TypeScript",
+    "Angular", "Vue", "Node.js", "Django", "Spring",
+    "Flask", "Laravel", "Express", "Sass", "Less",
+    "Bootstrap", "jQuery", "SQL", "NoSQL", "MongoDB",
+    "Firebase", "REST", "GraphQL", "API", "JSON",
+    "AJAX", "Webpack", "Babel", "Redux", "MobX",
+    "RxJS", "Deno", "Jest", "Mocha", "Chai",
+    "Enzyme", "Cypress", "Puppeteer", "Git", "GitHub"
+  ],
+  alimento: [
+    "Arroz", "Feijão", "Carne", "Leite", "Ovo",    
+    "Pão", "Macarrão", "Batata", "Cenoura", "Brócolis",
+    "Abóbora", "Beterraba", "Alface", "Tomate", "Pepino",
+    "Couve", "Espinafre", "Repolho", "Cebola", "Alho",
+    "Frango", "Peixe", "Salsicha", "Presunto", "Queijo",
+    "Iogurte", "Manteiga", "Azeite", "Óleo", "Açúcar",
+    "Sal", "Pimenta", "Canela", "Chocolate", "Café",
+    "Chá", "Suco", "Vinho", "Cerveja"
+  ]
+}
